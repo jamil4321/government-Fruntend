864b3ecb306e79fd6d322759770178f77a80e6fc
Hasaan Developer <107426718+muhammed-hasaan@users.noreply.github.com>
2024-01-29 19:35:55 +0500

    Add files via upload

---
+++ src/Components/LoginComponent/LoginComponent.jsx
@@ -0,0 +1,95 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import Swal from 'sweetalert2';
+
+
+export default function LoginComponent() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleLogin = (e) => {
+        e.preventDefault()
+        console.log("email -->", email)
+        console.log("password -->", password)
+
+        fetch("http://localhost:5000/login-user", {
+            method: "POST",
+            crossDomain: true,
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+                "Access-Control-Allow-Origin": "*",
+            },
+            body: JSON.stringify({
+                email,
+                password,
+            }),
+        })
+            .then((res) => res.json())
+            .then((data) => {
+
+                if (data) {
+
+                    localStorage.setItem("email", JSON.stringify(email))
+                    console.log(data, "userRegister");
+                    Swal.fire({
+                        title: "Login Successfully",
+                        icon: "success",
+                        showConfirmButton: false
+                    });
+                    setTimeout(() => {
+                        window.location = "/Dashboard"
+                    }, 2000);
+                }
+            })
+    }
+    return (
+        <div className="main" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', width: '100%', height: '100vh' }}>
+            <div className="login" style={{ maxWidth: '400px', width: '80%' }}>
+                <div className="child1">
+                    <h1 style={{ fontSize: '25px', fontWeight: 'bold' }}>Hello Again!</h1>
+                    <h4>Welcome Back</h4>
+                </div>
+                <div className="child1" style={{}}>
+                    <form onSubmit={handleLogin}>
+
+                        <input
+                            type="email"
+                            placeholder="Email Address"
+                            style={{ ...inputStyle, width: '100%' }}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                        <input
+                            type="password"
+                            placeholder="Password"
+                            style={{ ...inputStyle, marginTop: '15px', width: '100%' }}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <button style={buttonStyle}>Login</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+const inputStyle = {
+    border: '2px solid gray',
+    width: '100%',
+    height: '55px',
+    borderRadius: '30px',
+    paddingLeft: '20px',
+    marginTop: '15px',
+    backgroundColor: 'none',
+};
+
+const buttonStyle = {
+    border: '2px solid black',
+    width: '100%',
+    height: '55px',
+    borderRadius: '30px',
+    marginTop: '15px',
+    backgroundColor: '#023D20',
+    color: 'white',
+};
---
+++ src/Components/QRDetailComponent/QrDetailComponent.jsx
@@ -0,0 +1,133 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useEffect } from "react";
+import Swal from "sweetalert2";
+import { useParams } from 'react-router-dom';
+
+
+export default function QrDetailComponent() {
+  const [password, setPassword] = useState('')
+  const [allImage, setAllImage] = useState([]);
+  const [url, setUrl] = useState('');
+
+
+
+  
+  const { id } = useParams();
+
+  useEffect(() => {
+    var urid = id.slice(3, 17);
+    setUrl(urid);
+    getImage();
+  }, [id]); 
+
+
+  useEffect(() => {
+    getImage()
+  }, [])
+
+  function getImage() {
+    fetch("http://localhost:5000/get-image", {
+      method: "GET",
+    }).then((res) => res.json()).then((data) => {
+      console.log(data)
+      setAllImage(data.data)
+    })
+  }
+
+
+  const handleLogin = (e) => {
+    e.preventDefault()
+    allImage.map((item, index) => {
+
+      // const currentURL = window.location.href;
+      // const startIndex = 34
+      // const endIndex = 50 // Assuming the length is always 14 characters
+
+      // const urlid = currentURL.slice(startIndex, endIndex);
+      // console.log(currentURL, 'currentUrl');
+      // console.log(urlid);
+
+      if (item.Qrcode === url && item.password == password) {
+        localStorage.setItem("name", JSON.stringify(item.name))
+        localStorage.setItem("fileid", JSON.stringify(item.fileid))
+        localStorage.setItem("select", JSON.stringify(item.select))
+        localStorage.setItem("image", JSON.stringify(item.image))
+        localStorage.setItem("date", JSON.stringify(item.date))
+
+
+        console.log(item.Qrcode, "Qrcode");
+        Swal.fire({
+          title: "File Founded",
+          icon: "success",
+          customClass: {
+            popup: "left-popup", // Add a custom class to center the popup
+          }
+        });
+        setTimeout(() => {
+          window.location = "/FileView"
+        }, 3000);
+      } else {
+        Swal.fire({
+          title: "File Not Founded",
+          icon: "warning",
+          customClass: {
+            popup: "left-popup", // Add a custom class to center the popup
+          }
+        });
+      }
+    })
+
+  }
+
+  const autoName = JSON.parse(localStorage.getItem('QrDetailName'))
+
+
+  return (
+    <div className="main" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', width: '100%', height: '100vh' }}>
+      <div className="login" style={{ maxWidth: '400px', width: '80%' }}>
+        <div className="child1">
+          <h1 style={{ fontSize: '25px', fontWeight: 'bold' }}>Hello Again!</h1>
+          <h4>Find File</h4>
+        </div>
+        <div className="child1" style={{}}>
+          <form onSubmit={handleLogin}>
+            <input
+              type="password"
+              placeholder="Enter File Password"
+              style={{ ...inputStyle, marginTop: '15px', width: '100%' }}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button style={buttonStyle}>View File</button>
+            <center>
+
+              <p style={{ marginTop: '20px' }}>Forgot Password</p>
+              <p style={{ marginTop: '20px' }}><Link to="/Dashboard">Dashboard</Link></p>
+            </center>
+          </form>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+
+const inputStyle = {
+  border: '2px solid gray',
+  width: '100%',
+  height: '55px',
+  borderRadius: '30px',
+  paddingLeft: '20px',
+  marginTop: '15px',
+  backgroundColor: 'none',
+};
+
+const buttonStyle = {
+  border: '2px solid black',
+  width: '100%',
+  height: '55px',
+  borderRadius: '30px',
+  marginTop: '15px',
+  backgroundColor: '#023D20',
+  color: 'white',
+};
