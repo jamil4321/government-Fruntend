5ae57de06eaac1203089b67b0fa7c6aa13ed1195
Hasaan Developer <muhammedhasaan09@gmail.com>
2024-01-31 16:05:35 +0500

    Update QrDetailComponent.jsx

--- src/Components/QRDetailComponent/QrDetailComponent.jsx
+++ src/Components/QRDetailComponent/QrDetailComponent.jsx
@@ -1,91 +1,85 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import { useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom"; // Updated import
+// import { useHistory } from "react-router-dom";
 import Swal from "sweetalert2";
-import { useParams } from 'react-router-dom';
-
 
 export default function QrDetailComponent() {
-  const [password, setPassword] = useState('')
-  const [allImage, setAllImage] = useState([]);
+  const [password, setPassword] = useState('');
   const [url, setUrl] = useState('');
 
+  localStorage.setItem("name", JSON.stringify(""))
+  localStorage.setItem("fileid", JSON.stringify(""))
+  localStorage.setItem("select", JSON.stringify(""))
+  localStorage.setItem("image", JSON.stringify(""))
+  localStorage.setItem("date", JSON.stringify(""))
+  localStorage.setItem("location", JSON.stringify(""))  
 
-
-  
   const { id } = useParams();
-
   useEffect(() => {
-    var urid = id.slice(3, 17);
-    setUrl(urid);
-    getImage();
-  }, [id]); 
+      var urid = id.slice(3, 17);
+      setUrl(urid);
+    }, [id]); 
 
-
-  useEffect(() => {
-    getImage()
-  }, [])
-
-  function getImage() {
-    fetch("https://government-backend-production.up.railway.app/get-image", {
-      method: "GET",
-    }).then((res) => res.json()).then((data) => {
-      console.log(data)
-      setAllImage(data.data)
-    })
-  }
-
-
-  const handleLogin = (e) => {
-    e.preventDefault()
-    /* eslint-disable array-callback-return */
-    allImage.map((item, index) => {
-
-      // const currentURL = window.location.href;
-      // const startIndex = 34
-      // const endIndex = 50 // Assuming the length is always 14 characters
-
-      // const urlid = currentURL.slice(startIndex, endIndex);
-      // console.log(currentURL, 'currentUrl');
-      // console.log(urlid);
-
-      if (item.Qrcode === url && item.password === password) {
-        localStorage.setItem("name", JSON.stringify(item.name))
-        localStorage.setItem("fileid", JSON.stringify(item.fileid))
-        localStorage.setItem("select", JSON.stringify(item.select))
-        localStorage.setItem("image", JSON.stringify(item.image))
-        localStorage.setItem("date", JSON.stringify(item.date))
-
-
-        console.log(item.Qrcode, "Qrcode");
-        Swal.fire({
-          title: "File Founded",
-          icon: "success",
-          customClass: {
-            popup: "left-popup", // Add a custom class to center the popup
-          }
-        });
-        setTimeout(() => {
-          window.location = "/FileView"
-        }, 3000);
-      } else {
-        Swal.fire({
-          title: "File Not Founded",
-          icon: "warning",
-          customClass: {
-            popup: "left-popup", // Add a custom class to center the popup
+    
+    
+    
+      const handleLogin = async (e) => {
+        e.preventDefault();
+    
+        fetch("http://localhost:5000/get-image", {
+          method: "GET",
+        })
+        .then((res) => res.json())
+        .then((data) => {
+          let fileFounded = false;
+    
+          data.data.forEach((item) => {
+            if (item.Qrcode === url && item.password === password) {
+              fileFounded = true;
+              localStorage.setItem("name", JSON.stringify(item.name))
+            localStorage.setItem("fileid", JSON.stringify(item.fileid))
+            localStorage.setItem("select", JSON.stringify(item.select))
+             localStorage.setItem("image", JSON.stringify(item.image))
+            localStorage.setItem("date", JSON.stringify(item.date))
+            localStorage.setItem("location", JSON.stringify(item.location))  
+    
+              return; // Exit the loop once a match is found
+            }
+          });
+    
+          if (fileFounded) {
+            Swal.fire({
+              title: "File Founded",
+              icon: "success",
+              showConfirmButton:false,
+              customClass: {
+                popup: "left-popup",
+              },
+            });
+             setTimeout(() => {
+            window.location = "/FileView"
+          }, 2000);
+          } else {
+            Swal.fire({
+              title: "File Not Founded",
+              icon: "warning",
+              customClass: {
+                popup: "left-popup",
+              },
+              showConfirmButton: false
+            });
           }
         });
       }
-    })
-
-  }
-
-  // const autoName = JSON.parse(localStorage.getItem('QrDetailName'))
-
-
-  return (
-    <div className="main" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', width: '100%', height: '100vh' }}>
+    
+      // Rest of your component code...
+    
+    
+    
+    
+    
+    return (
+      <div className="main" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', width: '100%', height: '100vh' }}>
       <div className="login" style={{ maxWidth: '400px', width: '80%' }}>
         <div className="child1">
           <h1 style={{ fontSize: '25px', fontWeight: 'bold' }}>Hello Again!</h1>
@@ -98,20 +92,15 @@ export default function QrDetailComponent() {
               placeholder="Enter File Password"
               style={{ ...inputStyle, marginTop: '15px', width: '100%' }}
               onChange={(e) => setPassword(e.target.value)}
-            />
-            <button style={buttonStyle}>View File</button>
-            <center>
-
-              <p style={{ marginTop: '20px' }}>Forgot Password</p>
-              <p style={{ marginTop: '20px' }}><Link to="/Dashboard">Dashboard</Link></p>
-            </center>
+              />
+            <button style={buttonStyle} onClick={handleLogin}>View File</button>
+      
           </form>
         </div>
       </div>
     </div>
-  )
+  );
 }
-
 
 const inputStyle = {
   border: '2px solid gray',
