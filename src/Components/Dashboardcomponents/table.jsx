25ffb7276636187412621edf94770fddee41d038
Jamil Ahmed <jamil.ahmed.ja28@gmail.com>
2024-02-13 16:50:17 +0500

    update

--- src/Components/Dashboardcomponents/table.jsx
+++ src/Components/Dashboardcomponents/table.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable no-unused-vars */
+/* eslint-disable */
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { UserPlusIcon } from "@heroicons/react/24/solid";
 import Swal from 'sweetalert2';
