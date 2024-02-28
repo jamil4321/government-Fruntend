8a8d430c17663d202faa62e3c91d52fc77d0c454
Hasaan Developer <muhammedhasaan09@gmail.com>
2024-02-28 22:50:31 +0500

    Update Navbar.jsx

--- src/Components/Dashboardcomponents/Navbar.jsx
+++ src/Components/Dashboardcomponents/Navbar.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import React from "react";
 import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button } from "@nextui-org/react";
 import { Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Avatar, User } from "@nextui-org/react";
